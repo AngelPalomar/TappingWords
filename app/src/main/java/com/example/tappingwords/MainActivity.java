f142126a70d70b1e88fb3f08fb20ef5af89351cf
Angel Palomar <angelpg.123@hotmail.com>
2020-10-26 21:25:22 -0600

    Primer commit del proyecto.

---
+++ app/src/main/java/com/example/tappingwords/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tappingwords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnToHard, btnToNormal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnToNormal = findViewById(R.id.btn_to_normal);
+        btnToHard = findViewById(R.id.btn_to_hard);
+
+        //Iniciar modo normal
+        btnToNormal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NormalMode.class));
+            }
+        });
+
+        //Iniciar modo dificil
+        btnToHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HardMode.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/tappingwords/NormalMode.java
@@ -0,0 +1,14 @@
+package com.example.tappingwords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NormalMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_mode);
+    }
+}
